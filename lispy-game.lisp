5b35cb602a3b848a8390d50b827d6270991b62e4
Pratik Karki <prertik@outlook.com>
2017-07-29 23:37:23 +0545

    add some paths cases

--- lispy-game.lisp
+++ lispy-game.lisp
@@ -12,3 +12,5 @@
                        (attic (living-room downstairs ladder))))
 (defun describe-path (edge)
   `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
