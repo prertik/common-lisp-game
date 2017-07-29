ecf06b4c8013c855103a8a888d748df816fdbebb
Pratik Karki <prertik@outlook.com>
2017-07-29 17:14:29 +0545

    first things first

---
+++ lispy-game.lisp
@@ -0,0 +1,6 @@
+(defparameter *nodes* '((living-room (you are in the living room.
+                                      a wizard is snoring on the couch))
+                        (garden (you are in a beautiful garden.
+                                 there is a well in front of you.))
+                        (attic (you are in the attic.
+                                there is a giant wielding torch in the corner.))))
