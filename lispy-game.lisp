acc8e40efc61281c60060f70ae4a1b60b4bfc0b1
Pratik Karki <prertik@outlook.com>
2017-07-30 13:24:05 +0545

    add description of objects at location

--- lispy-game.lisp
+++ lispy-game.lisp
@@ -23,3 +23,7 @@
   (labels ((at-loc-p (obj)
              (eq (cadr (assoc obj obj-locs)) loc)))
     (remove-if-not #'at-loc-p objs)))
+(defun describe-object (loc objs obj-loc)
+  (labels ((describe-obj(obj)
+             `(you see a ,obj on the floor.)))
+    (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
